d83382b184edca967c0aca0e1463dff6ca66bd9e
Cody Pauley <cpgameface@gmail.com>
2018-05-31 04:56:10 +0000

    forms

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var nunjucks = require('nunjucks');
+const body_parser = require('body-parser');
 
+app.use(body_parser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 nunjucks.configure('views', {
@@ -14,6 +16,15 @@ app.get('/', function (request, response) {
   var name = request.query.name || 'World';
   var context = {title: 'Hello', name: name};
   response.render('index.html', context);
+});
+
+app.get('/form', function(request, response) {
+  response.render('form.html');
+});
+
+app.post('/submit', function (request, response) {
+  console.log(request.body);
+  response.send('OK');
 });
 
 app.listen(8080, function () {
